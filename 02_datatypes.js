87382a34c0d78e11151b843de5d5c4ce7b8774a9
Tanishkanikam <166438552+Tanishkanikam@users.noreply.github.com>
2025-06-05 07:39:57 +0000

    Added 02_datatypes.js

---
+++ 02_datatypes.js
@@ -0,0 +1,15 @@
+"use strict"
+
+console.log(3 + 3)
+console.log("tanishka")
+
+let name = " tanishla"
+let age = 3
+let isLoggedIn = false
+
+// number => 2 poer of 53
+// bigint 
+// string => " "
+// boolean => true/false
+// null => standalone value
+// undefined
