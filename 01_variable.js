16096e447fe772e9d7570a09c0a45a9201a1aaee
Tanishkanikam <166438552+Tanishkanikam@users.noreply.github.com>
2025-06-05 07:27:21 +0000

    Added 01_variable.js

---
+++ 01_variable.js
@@ -0,0 +1,11 @@
+const accountId = 123444
+let accountEmail = "tanishka@gmail.com"
+var accountPass = "1223"
+accountCity = "chali"
+
+accountEmail="vdgsv@gmail"
+accountPass= "6565362"
+accountCity = "bengluru"
+
+console.log(accountId)
+console.table(accountEmail, accountId , accountPass, accountCity)
