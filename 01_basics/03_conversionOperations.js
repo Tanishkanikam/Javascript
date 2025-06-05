a0395ba458aeb02b5a0c01746038348c930190b2
Tanishkanikam <166438552+Tanishkanikam@users.noreply.github.com>
2025-06-05 13:08:56 +0000

    comparision and coversions

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -23,5 +23,46 @@ console.log(booleanisLoggedIn);
 
 let someNumber = 33
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof (stringNumber));
+//console.log(stringNumber);
+//console.log(typeof (stringNumber));
+
+// ******* Operations *********
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+let str1 = "hello"
+let str2 = " tanishka"
+
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+console.log(1 + 2 + 2);
+console.log(1 + "2" + 2);
+
+console.log(true);
+console.log(-true);
+console.log(-false);
+
+let num1 , num2 , num3
+num1 = num2 = num3 = 2+2
+
+let gamecounter = 100
+gamecounter++
+console.log(gamecounter);
+
+
+
+
+
+
+
+
+
+
+
