df5cf6a2a71024ab803bdb8277d246d0e1f06572
Tanishkanikam <166438552+Tanishkanikam@users.noreply.github.com>
2025-06-19 13:31:23 +0000

    array

--- 02_basics/01_array.js
+++ 02_basics/01_array.js
@@ -4,4 +4,32 @@ const arry = [0, 1, 2, 3, 4, 5]
 const myHeors = ["shaktiman", "naagraj"]
 
 const arry2 = new Array(1, 2, 3, 4)
-console.log(arry[0]);
+console.log(arry[1]);
+
+arry.push(6)
+console.log(arry);
+arry.push(9)
+console.log(arry);
+
+arry.pop()
+console.log(arry);
+
+//arry.unshift(9)
+console.log(arry.includes(9));
+console.log(arry.indexOf(3));
+
+console.log(arry);
+
+//slice ,splice
+
+const myn1 = arry.slice(1, 3)
+
+console.log(myn1);
+console.log("B ", arry);
+
+const myn2 = arry.splice(1, 3)
+
+console.log(myn1);
+
+console.log(myn2);
+
