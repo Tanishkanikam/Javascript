69ebc0ce1ced7c33ec8ca0c1c47ac7b9dff84310
Tanishkanikam <166438552+Tanishkanikam@users.noreply.github.com>
2025-06-22 11:52:37 +0000

    functions

---
+++ 02_basics/05_functions.js
@@ -0,0 +1,38 @@
+//parse it to a number
+// function sum(a,b){
+//     return a+b;
+
+// }
+// let ans = sum("20" , 30)
+// console.log(ans);
+
+function  sum(n) {
+    let ans = 0;
+    for(let i =0; i<=n; i++) {
+        ans = ans + i;
+}
+return ans;
+}
+
+const ans1 = sum(100);
+console.log(ans1);
+const ans2 = sum(1000);
+console.log(ans2);
+const ans3 = sum(10000);
+console.log(ans3);
+
+function addition(a,b) {
+    return a + b;
+}
+
+function multiply (a,b){
+return a*b;
+}
+function subtract (a,b){
+return a-b;
+}
+function divide (a,b){
+return a/b;
+}
+
+console.log(ans);
---
+++ 02_basics/06_synchronous.js
@@ -0,0 +1,6 @@
+const fs = require('fs');
+
+const data = fs.readFileSync('a.txt', 'utf8');
+console.log(data);
+
+
